f475053f0f4e7d6f751516f3e43fa0e8599d7634
Bnayahu <bnayahu@arena-digital.com>
2026-07-23 16:12:42 +0300

    fix playmode

--- CharComponent.cs
+++ CharComponent.cs
@@ -13,12 +13,18 @@ public class CharComponent : MonoBehaviour {
         public SkinnedMeshRenderer ShirtRenderer;
         public Transform HeadTf;
         [Range(0f, 100f)] public float Weight = 100f;
-    }
+    } 
 
     static readonly int AV_MirrorHash = Animator.StringToHash("is_mirror");
+    static readonly int AV_CycleOffset = Animator.StringToHash("cycle_offset");
     static readonly Color _EnemyColor = Color.softRed;
-    static readonly Color _AllyColor = Color.white;  
-
+    static readonly Color _AllyColor = Color.white;
+    static bool IsExportEditor =>
+#if DRILL_EXPORT_EDITOR
+        !Application.isPlaying;
+#else
+        false;
+#endif
     [ShowInInspector, SerializeField] CharData _data;
     [SerializeField] CharSkin[] _skins;
 
@@ -32,24 +38,21 @@ public class CharComponent : MonoBehaviour {
     public AnimationClip Clip => _overrideAnimation ? _overrideAnimation.animationClips[0] : null;  
     public void SetData(CharData data, bool mirror) {
         _data = data;
+
+        if(IsExportEditor && !_graph.IsValid())
+            RecreateGraph();
+
         if(data != null) { 
             var pos = new Vector3(data.FieldStandardPosition.y,0,data.FieldStandardPosition.x);
             var rot = Quaternion.Euler(0, data.yRotation, 0);
             transform.SetLocalPositionAndRotation(pos, rot);
-            _controllerPlayable.SetBool(AV_MirrorHash, mirror);
             SetAnim(data.Animation);
+            if(_controllerPlayable.IsValid())
+                _controllerPlayable.SetBool(AV_MirrorHash, mirror);
                 
             _skins[_usedAnimatorSkinIdx].ShirtRenderer.sharedMaterial.color = data.IsFriendly ? _AllyColor : _EnemyColor;
         }
-    }
-
-
-    private void Awake() {
-        foreach(var skin in _skins) {
-            skin.ShirtRenderer.sharedMaterial = new Material(skin.ShirtRenderer.sharedMaterial);
-        }
-        RandomizeSkin();
-    }
+    } 
 
     public void RandomizeSkin() {
         if (_skins.Length > 0) {
@@ -78,47 +81,68 @@ public class CharComponent : MonoBehaviour {
     }
 
     PlayableGraph _graph;
-    AnimationPlayableOutput _output;
-    private void OnEnable() { 
-        _anim.fireEvents = false;
-        _anim.speed = 0f;
-        RecreateGraph(); 
-    }
+    AnimationPlayableOutput _output; 
+    private void Start() {
+        foreach (var skin in _skins) {
+            skin.ShirtRenderer.sharedMaterial = new Material(skin.ShirtRenderer.sharedMaterial);
+        }
+        RandomizeSkin();
+         
+        foreach (var s in _skins) {
+            s.Animator.keepAnimatorStateOnDisable = true;
+            s.Animator.fireEvents = false;
+            s.Animator.enabled = true;
+            s.Animator.speed = 1f;
+        }
+        RecreateGraph();
+    } 
     AnimatorControllerPlayable _controllerPlayable;
-    AnimatorOverrideController _overrideAnimation;
-    void RecreateGraph() {  
+    AnimatorOverrideController _overrideAnimation; 
+    void RecreateGraph() { 
         _overrideAnimation = new AnimatorOverrideController(_anim.runtimeAnimatorController);
         _anim.runtimeAnimatorController = _overrideAnimation;
+        SetAnim(Data?.Animation);
 
-        if(_graph.IsValid())
-            _graph.Destroy();
+        if (IsExportEditor) {
+            if (_graph.IsValid())
+                _graph.Destroy();
 
-        _graph = PlayableGraph.Create("SingleAnimationGraph");
-        _graph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
-        _controllerPlayable = AnimatorControllerPlayable.Create(_graph, _overrideAnimation);
-        _output = AnimationPlayableOutput.Create(_graph, "AnimationOutput", _anim);
-        _output.SetSourcePlayable(_controllerPlayable);
-        SetAnim(null); 
-        _graph.Play();
+            _graph = PlayableGraph.Create("SingleAnimationGraph");
+            _graph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
+            _controllerPlayable = AnimatorControllerPlayable.Create(_graph, _overrideAnimation);
+            _output = AnimationPlayableOutput.Create(_graph, "AnimationOutput", _anim);
+            _output.SetSourcePlayable(_controllerPlayable);
+            _graph.Play();
+        }
     }
 
     private void OnDestroy() {
-        if (_graph.IsValid())
+        if (IsExportEditor && _graph.IsValid())
             _graph.Destroy();
     } 
 
     public void SetAnimationTime(float time) {
-        if (!_graph.IsValid() || !_controllerPlayable.IsValid())
-            return;
-
-        if (_controllerPlayable.IsValid()) {
-            var actualTime = time + (_data?.AnimationTimeOffset ?? 0f);
-            var duration = _overrideAnimation.animationClips[0].length;
-            var normalized = (duration == 0f || actualTime < 0f) ? 0f : actualTime / duration;
-            _controllerPlayable.Play("Clip", 0, normalized); 
+        if (IsExportEditor) {
+            if (!_graph.IsValid() || !_controllerPlayable.IsValid())
+                return;
         }
-        if(_graph.IsValid())
-            _graph.Evaluate(); 
+
+        if (!_overrideAnimation)
+            RecreateGraph();
+
+        var actualTime = time + (_data?.AnimationTimeOffset ?? 0f);
+        var clip = _overrideAnimation.animationClips[0];
+        var duration = clip ? clip.length : 0f;
+        var normalized = (duration == 0f || actualTime < 0f) ? 0f : actualTime / duration;
+
+        if (IsExportEditor) {
+            _controllerPlayable.Play("Clip", 0, normalized);
+            _controllerPlayable.SetFloat(AV_CycleOffset, normalized);
+            _graph.Evaluate();
+        } else {
+            _anim.SetFloat(AV_CycleOffset, normalized);
+            _anim.Update(0f);
+        }
     } 
     void SetAnim(AnimationClip clip) {
         if (clip == Clip)
